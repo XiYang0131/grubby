7ac180ad9ce0cd49fe89e5efae9093518eca5160
XiYang0131 <448749018@qq.com>
2025-04-19 16:25:36 +0800

    11

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
   
   // 临时禁用严格模式
   reactStrictMode: false,
+  
+  // 添加webpack配置
+  webpack: (config) => {
+    // 忽略favicon.ico相关错误
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // 重定向favicon.ico请求到一个空模块
+      './favicon.ico': require.resolve('next/dist/lib/empty-object.js'),
+    };
+    
+    return config;
+  },
 };
 
 module.exports = nextConfig;
